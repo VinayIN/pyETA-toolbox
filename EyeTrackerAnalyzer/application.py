ca35de8339bb7c2e5df444393325fa75e6d21f99
VinayIN <bpradhan.asli@gmail.com>
2024-07-31 13:14:46 +0200

    modified layout

--- EyeTrackerAnalyzer/application.py
+++ EyeTrackerAnalyzer/application.py
@@ -1,9 +1,9 @@
 import sys
 import asyncio
 import multiprocessing
-from EyeTrackerAnalyzer.component.window import run_validation_window
+import dash
+from EyeTrackerAnalyzer.components.window import run_validation_window
 import dash_bootstrap_components as dbc
-from dash import Dash, html, dcc
 from dash.dependencies import Input, Output
 
 def run_async_function(async_func):
@@ -12,13 +12,34 @@ def run_async_function(async_func):
     loop.run_until_complete(async_func())
     loop.close()
 
-app = Dash(__package__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(__package__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = dbc.Container([
-    dbc.Row(dbc.Col(html.H1("Eye Tracker Analyzer"))),
-    dbc.Row(dbc.Col(html.Button("Open Grid", id="open-grid-window"))),
-    dbc.Row(dbc.Col(dcc.Input(id="input-box", type="text"))),
-    dbc.Row(dbc.Col(html.Div(id="output-box")))
+    dbc.Row(dash.html.H1("Eye Tracker Analyzer")),
+    dbc.Row([
+        dbc.Col(
+            dbc.Row([
+                dbc.Col(dash.dcc.Input(id="input-box", type="text")),
+                dbc.Col(dash.html.Div(id="output-box"))
+            ])),
+        dbc.Col(
+            dbc.Row([
+                dbc.Button("Validate Eye Tracker", id="open-grid-window")
+            ])),
+        ]),
+    dbc.Tabs([
+        dbc.Tab(label="Gaze points", tab_id="eye-tracker-gaze"),
+        dbc.Tab(label="Fixation", tab_id="eye-tracker-fixation"),
+    ],
+    id="tabs",
+    active_tab="eye-tracker-gaze"),
+    dbc.Spinner(
+            [
+                dash.dcc.Store(id="store"),
+                dash.html.Div(id="tab-content", className="p-4"),
+            ],
+            delay_show=100,
+        ),
 ])
 
 @app.callback(
@@ -26,7 +47,7 @@ app.layout = dbc.Container([
     Input('input-box', 'value')
 )
 def update_output(value):
-    return f'Input: {value}'
+    return f'Participant No: {value}'
 
 @app.callback(
     Output('open-grid-window', 'n_clicks'),
@@ -38,6 +59,19 @@ def update_window(n_clicks):
         process = multiprocessing.Process(target=run_async_function, args=(run_validation_window,))
         process.start()
     return n_clicks
+
+@app.callback(
+    Output("tab-content", "children"),
+    [Input("tabs", "active_tab"), Input("store", "data")],
+)
+def render_tab_content(active_tab, data):
+    if active_tab == "eye-tracker-gaze":
+        print("plotting gaze points")
+        return "Gaze tab"
+    elif active_tab == "eye-tracker-fixation":
+        print("plotting fixation points")
+        return "Fixation tab"
+    return "No tab selected"
 
 
 if __name__ == '__main__':
